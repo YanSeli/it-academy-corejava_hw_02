c0ceba64096e7169396fa6eb905cfccbe030800c
Y.Selivanau <yan.selivanov@g,ail.com>
2019-07-01 18:53:58 +0300

    -last task

--- src/ru/javabegin/training/fastjava2/Lesson2/task5_38.java
+++ src/ru/javabegin/training/fastjava2/Lesson2/task5_38.java
@@ -5,5 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class task5_38 {
+    public static void main(String[] args) throws IOException {
+        int way = 1000; //расстояние в метрах
+        int wayAfter1Iter = way + (way/2) + 130 + 140;
+        int n = 100;
+        int allWay = wayAfter1Iter * (100/4);
+        int w = 0;
+        for (int i = 0; i < n; i++){
+
+        }
+    //Вариант б)
+        System.out.println("Всего прошел за " + n + " этапов " + allWay/1000 + " км.");
+    //Вариант а)
+    //System.out.println("После " + n + " этапов, находится на расстоянии " + w/1000 + " км. от дома.");
 
     }
+}
